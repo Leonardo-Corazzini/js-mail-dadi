950ca3b45ac926d497a0fdd8d3699d20f78ff878
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-17 17:05:45 +0200

    FIX: impostato che si ha accesso al gioco solo se l'email è associata all'account e migliorato interazione con utente aggiungendo allert

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // traccia uno
 const emailutente = prompt('Inserisci la tua email')
-const emailRegistrate = ['leocora10@gmail.com','ronaldogoat7@gmail.com']
+const emailRegistrate = ['leocora10@gmail.com','ronaldogoat7@gmail.com','mikicora10@gmail.com']
 let emailRegistrata = false
 for(let i = 0; i < emailRegistrate.length; i++){
     if (emailRegistrate[i]=== emailutente){
@@ -10,24 +10,24 @@ for(let i = 0; i < emailRegistrate.length; i++){
    
 if (emailRegistrata === true){
     console.log('Bentornato caro utente')
+    // traccia due
+    alert('Bentornato ti sfido al gioco dei dadi! Guarda la console per scoprire chi ha vinto.')
+    const numeroUtente = Math.floor(Math.random() * 6)
+    console.log(`Questo è il TUO numero: ${numeroUtente}`)
+    const numeroPc =Math.floor(Math.random() * 6)
+    console.log(`Questo è il MIO numero: ${numeroPc}`)
+    if (numeroUtente > numeroPc){
+    console.log('Bravo hai vinto')
+    } else if (numeroUtente === numeroPc){
+    console.log('ah abbiamo pareggiato!')
+    } else {
+    console.log('hahahaha hai perso')
+    }
 } else {
     console.log('La email inserita non è associata a nessun account')
 }
     
 
 
-// traccia due
-const numeroUtente = Math.floor(Math.random() * 6)
-console.log(numeroUtente)
-const numeroPc =Math.floor(Math.random() * 6)
-console.log(numeroPc)
-if (numeroUtente > numeroPc){
-    console.log('Bravo hai vinto')
-    
-} else if (numeroUtente === numeroPc){
-    console.log('ah abbiamo pareggiato!')
-} else {
-    console.log('hahahaha hai perso')
-}
- 
+
 
